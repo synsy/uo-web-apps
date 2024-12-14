60885ad151250205f6d4813638c0dd5c6bbd1418
synsy <57045115+synsy@users.noreply.github.com>
2024-12-13 18:38:34 -0800

    Added Progress Bar to XP
    Added a progress bar to XP tracker and functionality to make aspect and tier static across pages.

--- src/context/XpContext.jsx
+++ src/context/XpContext.jsx
@@ -6,8 +6,32 @@ export const XpProvider = ({ children }) => {
   const [aspectXp, setAspectXp] = useState({ start: 0, end: 0 });
   const [chainXp, setChainXp] = useState({ start: 0, end: 0 });
 
+  const [firstDropdown, setFirstDropdown] = useState("");
+  const [secondDropdown, setSecondDropdown] = useState("");
+  const [numberDropdown, setNumberDropdown] = useState(0);
+  const [secondTierDropdown, setSecondTierDropdown] = useState(0);
+
+  const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
+
   return (
-    <XpContext.Provider value={{ aspectXp, setAspectXp, chainXp, setChainXp }}>
+    <XpContext.Provider
+      value={{
+        aspectXp,
+        setAspectXp,
+        chainXp,
+        setChainXp,
+        firstDropdown,
+        setFirstDropdown,
+        secondDropdown,
+        setSecondDropdown,
+        numberDropdown,
+        setNumberDropdown,
+        secondTierDropdown,
+        setSecondTierDropdown,
+        isCheckboxChecked,
+        setIsCheckboxChecked,
+      }}
+    >
       {children}
     </XpContext.Provider>
   );
--- src/pages/XpTracker.jsx
+++ src/pages/XpTracker.jsx
@@ -3,22 +3,39 @@ import { XpContext } from "../context/XpContext";
 import "./XpTracker.css";
 
 const XpTracker = () => {
-  const { aspectXp, setAspectXp, chainXp, setChainXp } = useContext(XpContext);
+  const {
+    aspectXp,
+    setAspectXp,
+    chainXp,
+    setChainXp,
+    firstDropdown,
+    setFirstDropdown,
+    secondDropdown,
+    setSecondDropdown,
+    numberDropdown,
+    setNumberDropdown,
+    secondTierDropdown,
+    setSecondTierDropdown,
+    isCheckboxChecked,
+    setIsCheckboxChecked,
+  } = useContext(XpContext);
 
   const [startingAspectXp, setStartingAspectXp] = useState(aspectXp.start);
   const [endingAspectXp, setEndingAspectXp] = useState(aspectXp.end);
   const [startingChainXp, setStartingChainXp] = useState(chainXp.start);
   const [endingChainXp, setEndingChainXp] = useState(chainXp.end);
 
-  const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
-  const [firstDropdown, setFirstDropdown] = useState("");
-  const [secondDropdown, setSecondDropdown] = useState("");
-  const [numberDropdown, setNumberDropdown] = useState(0);
-  const [secondTierDropdown, setSecondTierDropdown] = useState(0);
+  const dropdownOptions = [
+    "Air", "Arcane", "Artisan", "Blood", "Command", "Death", "Discipline", "Earth", "Eldritch", "Fire", "Fortune",
+    "Frost", "Gadget", "Harvest", "Holy", "Lightning", "Lyric", "Madness", "Poison", "Shadow", "Void", "Water", "War"
+  ];
 
-  const dropdownOptions = ["Air", "Arcane", "Artisan", "Blood", "Command", "Death", "Discipline", "Earth", "Eldritch", "Fire", "Fortune",
-                           "Frost", "Gadget", "Harvest", "Holy", "Lightning", "Lyric", "Madness", "Poison", "Shadow", "Void", "Water", "War"];
-  
+  // const aspectColors = {
+  //   Air: "white", Arcane: "purple", Artisan: "gold", Blood: "red", Command: "green", Death: "grey", Discipline: "gold", Earth: "brown", 
+  //   Eldritch: "purple", Fire: "red", Fortune: "yellow", Frost: "blue", Gadget: "bronze", Harvest: "green", Holy: "yellow", Lightning: "yellow", 
+  //   Lyric: "pink", Madness: "red", Poison: "green", Shadow: "grey", Void: "purple", War: "orange", Water: "blue",
+  // };
+
   const xpTierAmounts = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 15000, 25000, 40000, 120000, 250000];
 
   const formatNumber = (number) => new Intl.NumberFormat().format(number);
@@ -47,6 +64,10 @@ const XpTracker = () => {
 
   const aspectXpGained = Math.max(0, endingAspectXp - startingAspectXp);
   const chainXpGained = Math.max(0, endingChainXp - startingChainXp);
+  const tierXpValue = xpTierAmounts[numberDropdown] || 0;
+  const aspectXpPercentage = endingAspectXp > startingAspectXp 
+    ? Math.min(100, Math.round((endingAspectXp / tierXpValue) * 100))
+    : 0;
 
   return (
     <div className="xp-tracker-container">
@@ -80,20 +101,20 @@ const XpTracker = () => {
           </label>
 
           <div className="number-dropdown">
-          <label>
-            Tier Level:
-            <select
-              value={numberDropdown}
-              onChange={(e) => setNumberDropdown(e.target.value)}
-            >
-              {Array.from({ length: 16 }, (_, i) => i).map((num) => (
-                <option key={num} value={num}>
-                  {num}
-                </option>
-              ))}
-            </select>
-          </label>
-        </div>
+            <label>
+              Tier Level:
+              <select
+                value={numberDropdown}
+                onChange={(e) => setNumberDropdown(e.target.value)}
+              >
+                {Array.from({ length: 15 }, (_, i) => i).map((num) => (
+                  <option key={num} value={num}>
+                    {num}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
 
           {isCheckboxChecked && (
             <>
@@ -120,7 +141,7 @@ const XpTracker = () => {
                   value={secondTierDropdown}
                   onChange={(e) => setSecondTierDropdown(e.target.value)}
                 >
-                  {Array.from({ length: 16 }, (_, i) => i).map((num) => (
+                  {Array.from({ length: 15 }, (_, i) => i).map((num) => (
                     <option key={num} value={num}>
                       {num}
                     </option>
@@ -153,6 +174,12 @@ const XpTracker = () => {
               onChange={(e) => handleAspectChange("end", e.target.value)}
               min="0"
             />
+          </div>
+
+          <div className="progress-bar-container">
+            <div className="progress-bar" style={{ width: `${aspectXpPercentage}%` }}>
+              {aspectXpPercentage}%
+            </div>
           </div>
         </div>
 
